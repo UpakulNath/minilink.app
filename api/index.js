59bdaae903f684a87e00de95ef9f60b5f24da808
Upakul <upakuln1@gmail.com>
2025-12-23 19:43:56 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -1,11 +1,8 @@
-
 import app from "../src/app.js";
 import connectDatabase from "../src/db/index.js";
 
-
 connectDatabase().catch((err) => {
-  console.error("Failed to connect DB", err);
+  console.error("DB INIT FAILED:", err.message);
 });
-console.log("MONGO_DB_URI:", process.env.MONGO_DB_URI);
 
 export default app;
--- src/db/index.js
+++ src/db/index.js
@@ -3,21 +3,17 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 const connectDatabase = async () => {
-  
+  console.log("ENV CHECK MONGO_DB_URI:", process.env.MONGO_DB_URI);
+
   if (!process.env.MONGO_DB_URI) {
     throw new Error("MONGO_DB_URI is missing at runtime");
   }
 
   if (isConnected) return;
 
-  try {
-    await mongoose.connect(process.env.MONGO_DB_URI);
-    isConnected = true;
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error", error);
-    throw error;
-  }
+  await mongoose.connect(process.env.MONGO_DB_URI);
+  isConnected = true;
+  console.log("MongoDB connected");
 };
 
 export default connectDatabase;
