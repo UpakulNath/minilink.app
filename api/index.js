e0c67e94aae280e9caa6c391f8dd84af68b1baae
Upakul <upakuln1@gmail.com>
2025-12-23 19:11:53 +0530

    fix bugs

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
-import dotenv from "dotenv";
+
 import app from "../src/app.js";
 import connectDatabase from "../src/db/index.js";
 
-dotenv.config();
+
 connectDatabase().catch((err) => {
   console.error("Failed to connect DB", err);
 });
+console.log("MONGO_DB_URI:", process.env.MONGO_DB_URI);
 
 export default app;
