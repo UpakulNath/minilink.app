aa60644f3f095290620c7d8dc43ac9311fb43b38
Upakul <upakuln1@gmail.com>
2025-12-25 19:21:55 +0530

    test

--- api/index.js
+++ api/index.js
@@ -1,8 +1,26 @@
-import app from "../src/app.js";
-import connectDatabase from "../src/db/index.js";
+import express from "express";
+import path from "path";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-connectDatabase().catch((err) => {
-  console.error("DB INIT FAILED:", err.message);
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
+
+// serve frontend
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
+
+// test API
+app.get("/api/test", (req, res) => {
+  res.json({ ok: true });
 });
 
 export default app;
