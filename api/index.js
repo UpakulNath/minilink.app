292e5f87130a5f41751debc916c0e7ec88dc0116
Upakul <upakuln1@gmail.com>
2025-12-25 19:59:21 +0530

    make changes

--- api/index.js
+++ api/index.js
@@ -11,16 +11,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({ origin: true, credentials: true }));
 
-// serve frontend
-app.use(express.static(path.join(process.cwd(), "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "public", "index.html"));
-});
-
-// test API
-app.get("/api/test", (req, res) => {
-  res.json({ ok: true });
-});
 
 export default app;
